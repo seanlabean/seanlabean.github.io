30274984735c23deed8c05c13da1fdb01b287ae5
Sean Lewis <sean.phys@gmail.com>
2025-05-31 19:56:36 -0400

    *

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def write_header(fn):
         f.write(f"<title>{NAME}&mdash;{fn}</title></head>")
         f.write("<body>")
         if fn == "home":
-            f.write("<header><a href='home.html'><img src='../media/main.png' width='160' height='80'></a>&nbsp;&nbsp;&nbsp;&nbsp;<img src='../media/e3.JPG' alt='2024 eclipse timelapse by Ashley Lian' width='600' height='55'></header>") #<img src='../media/slewis_wiki.gif' width='600' height='80'><br></header>")
+            f.write("<header><a href='home.html'><img src='../media/main.png' width='160' height='80'></a>&nbsp;&nbsp;&nbsp;&nbsp;<img src='../media/e3.JPG' alt='2024 eclipse timelapse by Ashley Lian' width='900' height='82.5'></header>") #<img src='../media/slewis_wiki.gif' width='600' height='80'><br></header>")
         else:
-            f.write(f"<header><a href='home.html'><img src='../media/main.png' width='160' height='80'></a>&nbsp;&nbsp;&nbsp;&nbsp;<img src='../media/e3.JPG' alt='2024 eclipse timelapse by Ashley Lian' width='600' height='55'></header>")
+            f.write(f"<header><a href='home.html'><img src='../media/main.png' width='160' height='80'></a>&nbsp;&nbsp;&nbsp;&nbsp;<img src='../media/e3.JPG' alt='2024 eclipse timelapse by Ashley Lian' width='900' height='82.5'></header>")
         # can loop over header lines and do specific things based on contents
         #for line in head:
             #f.write(line)
